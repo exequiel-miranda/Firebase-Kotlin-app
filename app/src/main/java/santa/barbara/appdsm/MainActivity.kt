10032ebca6909eb4f674d127426cb98595c8f3a6
Bryan <Bryan_miranda@ricaldone.edu.sv>
2024-05-29 15:13:59 -0600

    Firebase integration

--- app/src/main/java/santa/barbara/appdsm/MainActivity.kt
+++ app/src/main/java/santa/barbara/appdsm/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // menu should be considered as top level destinations
         navView.setupWithNavController(navController)
     }
 }
